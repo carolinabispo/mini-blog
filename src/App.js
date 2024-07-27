886ee77481f301e9b6e8704cc5b59f10eff0a6fc
Carolina Bispo <carolinabispos07@gmail.com>
2024-07-27 11:55:25 -0300

    alteracoes firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+// hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
+// context
+import { AuthProvider } from "./context/AuthContext";
+
+// pages
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import NavBar from "./components/navbar/NavBar";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost.js/CreatePost";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => (setUser(user)))
+  },[auth])
+
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+          <NavBar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={!user ? <Login/> : <Navigate to="/" />} />
+              <Route path="/register" element={!user ? <Register/> : <Navigate to="/login" />} />
+              <Route path="/posts/create" element={!user ? <CreatePost/> : <Navigate to="login"/>}/>
+              <Route path="/dashboard" element={user ? <Dashboard/> : <Navigate to="/login" />}/>
+
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0dh8K8e6X36V5AMjsy-DVik4uvdL96vk",
+  authDomain: "mini-blog-fc6b2.firebaseapp.com",
+  projectId: "mini-blog-fc6b2",
+  storageBucket: "mini-blog-fc6b2.appspot.com",
+  messagingSenderId: "157189402867",
+  appId: "1:157189402867:web:13545db34c044e740c4786",
+  measurementId: "G-CRL8YKW442",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+export { db };
